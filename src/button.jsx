c5e900a031c7aa6a9a85d541980ca1a93491da48
Mike Walker <killyosaur@gmail.com>
2019-04-24 07:08:45 -0400

    created svg button icons

--- src/button.jsx
+++ src/button.jsx
@@ -26,3 +26,10 @@ ButtonComponent.propTypes = {
 };
 
 export {ButtonComponent};
+/*
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 65 65"><path fill="currentColor" d="M5 5 h10 v10 h-10 zm0 15 h10 v10 h-10 zm0 15 h10 v10 h-10 zm0 15 h10 v10 h-10 zm15 0 h40 v10 h-40zm0 -15 h40 v10 h-40zm0 -15 h40 v10 h-40zm0 -15 h40 v10 h-40z" /></svg>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 45 65"><path fill="currentColor" d="M12 7.5 l25 25 l-25 25 l-7.07107 -7.07107 l17.92893 -17.92893 l-17.92893 -17.92893Z" /></svg>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 45 65"><path fill="currentColor" d="M32 7.5 l-25 25 l25 25 l7.07107 -7.07107 l-17.92893 -17.92893 l17.92893 -17.92893Z" /></svg>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 65 65"><path fill="currentColor" d="M12 7.5 l25 25 l-25 25 l-7.07107 -7.07107 l17.92893 -17.92893 l-17.92893 -17.92893zm20 0 l25 25 l-25 25 l-7.07107 -7.07107 l17.92893 -17.92893 l-17.92893 -17.92893Z" /></svg>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 65 65"><path fill="currentColor" d="M32 7.5 l-25 25 l25 25 l7.07107 -7.07107 l-17.92893 -17.92893 l17.92893 -17.92893zm20 0 l-25 25 l25 25 l7.07107 -7.07107 l-17.92893 -17.92893 l17.92893 -17.92893Z" /></svg>
+*/
